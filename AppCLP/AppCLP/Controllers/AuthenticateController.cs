3b7e75852f22737a59a814be56359a8556a9993f
Wellington de Sousa Reis <wsr.coder@gmail.com>
2024-10-21 07:08:37 -0300

    Estrutura base do projeto em dotnet

---
+++ AppCLP/AppCLP/Controllers/AuthenticateController.cs
@@ -0,0 +1,20 @@
+﻿using AppCLP.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCLP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetUsers")]
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+
+            return users;
+        }
+    }
+}
